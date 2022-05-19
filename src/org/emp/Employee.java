db8e6208c79d8ebc272af04b16cfbbe386c916a7
chandrumms <105387646+chandrumms@users.noreply.github.com>
2022-05-19 11:53:15 +0530

    ft

---
+++ src/org/emp/Employee.java
@@ -0,0 +1,37 @@
+package org.emp;
+
+public class Employee {
+private void empId() {
+	// TODO Auto-generated method stub
+System.out.println("11006219");
+}
+private void empName() {
+	// TODO Auto-generated method stub
+System.out.println("chandru");
+}
+private void empDop() {
+	// TODO Auto-generated method stub
+System.out.println("service");
+	
+}
+private void empPhone() {
+	// TODO Auto-generated method stub
+System.out.println("9844789522");
+}
+private void empEmail() {
+	// TODO Auto-generated method stub
+System.out.println("chandrucn.sekar@gmail.com");
+}
+public static void main(String[] args) {
+	Employee e = new Employee();
+	e.empId();
+	e.empDop();
+	e.empEmail();
+	e.empName();
+	e.empId();
+	e.empPhone();
+}
+
+
+
+}
